3a1bbd023b29d3b36f3cfbd99c7b4d69770321ba
Cal Nightingale <cal.nightingale@gmail.com>
2025-02-22 11:11:38 -0700

    instance validation/uid caching

--- cloud_optimized_dicom/instance.py
+++ cloud_optimized_dicom/instance.py
@@ -1,23 +1,38 @@
+import logging
 import os
+import tarfile
 import tempfile
+from dataclasses import dataclass, field
 
+import pydicom
 from smart_open import open as smart_open
+
+from cloud_optimized_dicom.utils import DICOM_PREAMBLE, find_pattern
+
+logger = logging.getLogger(__name__)
 
 REMOTE_IDENTIFIERS = ["http", "s3://", "gs://"]
 
 
+@dataclass
 class Instance:
     """
     Object representing a single DICOM instance.
     """
 
-    def __init__(self, dicom_uri: str, transport_params: dict = {}):
-        self.dicom_uri = dicom_uri
-        self.transport_params = transport_params
+    dicom_uri: str
+    transport_params: dict = field(default_factory=dict)
+
+    # private/cached internal fields
+    _instance_uid: str = None
+    _series_uid: str = None
+    _study_uid: str = None
+
+    def __post_init__(self):
         if self.is_remote():
             self._local_path = None
         else:
-            self._local_path = dicom_uri
+            self._local_path = self.dicom_uri
 
     def is_remote(self) -> bool:
         """
@@ -47,6 +62,45 @@ class Instance:
                 uri=self.dicom_uri, mode="rb", transport_params=self.transport_params
             ) as source:
                 local_file.write(source.read())
+        self.validate()
+
+    def validate(self):
+        """
+        Open the instance, read the internal fields, and (TODO) validate they match hints if provided
+        """
+        with self.open() as f:
+            with pydicom.dcmread(f) as ds:
+                self._instance_uid = ds.SOPInstanceUID
+                self._series_uid = ds.SeriesInstanceUID
+                self._study_uid = ds.StudyInstanceUID
+
+    @property
+    def instance_uid(self):
+        """
+        Getter for self._instance_uid. Populates by calling self.validate() if necessary.
+        """
+        # fetch if necessary
+        if self._instance_uid is None:
+            self.validate()
+        return self._instance_uid
+
+    @property
+    def series_uid(self):
+        """
+        Getter for self._series_uid. Populates by calling self.validate() if necessary.
+        """
+        if self._series_uid is None:
+            self.validate()
+        return self._series_uid
+
+    @property
+    def study_uid(self):
+        """
+        Getter for self._study_uid. Populates by calling self.validate() if necessary.
+        """
+        if self._study_uid is None:
+            self.validate()
+        return self._study_uid
 
     def open(self):
         """
@@ -54,6 +108,46 @@ class Instance:
         """
         self.fetch()
         return open(self._local_path, "rb")
+
+    def append_to_series_tar(
+        self, tar: tarfile.TarFile, delete_local_on_completion: bool = False
+    ):
+        """
+        Append the instance to a series tar file. Intended use case:
+        ```
+        with tarfile.open("series.tar", "w") as tar:
+            instance.append_to_series_tar(tar)
+        ```
+        Args:
+            tar: tarfile.TarFile to append to
+            delete_local_on_completion: if True and dicom_uri is local, delete the local instance file on completion
+        """
+        # do actual appending
+        f = tar.fileobj
+        begin_offset = f.tell()
+        tar.add(self._local_path, arcname=f"/instances/{self.deid_instance_uid}.dcm")
+        end_offset = f.tell()
+        f.seek(begin_offset)
+        # TODO: if index is always 1536, we can skip the find pattern
+        index = find_pattern(f, DICOM_PREAMBLE)
+        if index != 1536:
+            logger.warning(f"Unexpected DICOM tar header size {index}")
+        if index == -1:
+            raise ValueError("Not a Valid DICOM")
+        start_offset = begin_offset + index
+        stop_offset = start_offset + self.size
+        # Seek back to the end offset to read the file from the tar
+        f.seek(end_offset)
+
+        # set byte offsets
+        self.byte_offsets = (start_offset, stop_offset)
+        # cleanup temp file if exists (no longer needed)
+        self.cleanup()
+        # delete local origin if flag is set
+        if delete_local_on_completion and not self.is_remote:
+            os.remove(self._local_path)
+        # point local_origin_file within the local tar
+        self._local_path = f"{tar.name}://instances/{self.deid_instance_uid}.dcm"
 
     def cleanup(self):
         """
--- cloud_optimized_dicom/tests/test_instance.py
+++ cloud_optimized_dicom/tests/test_instance.py
@@ -1,4 +1,5 @@
 import os
+import tarfile
 import unittest
 
 import pydicom
@@ -38,3 +39,29 @@ class TestInstance(unittest.TestCase):
                 ds.SOPInstanceUID,
                 "1.2.276.0.7230010.3.1.4.2927855660.2552.1497110443.461491",
             )
+
+    def test_validate(self):
+        instance = Instance(os.path.join(self.test_data_dir, "small_multiframe.dcm"))
+        self.assertIsNone(instance._instance_uid)
+        self.assertIsNone(instance._series_uid)
+        self.assertIsNone(instance._study_uid)
+        instance.validate()
+        # after validation, the internal fields should be populated
+        self.assertEqual(
+            instance._instance_uid, "1.3.12.2.1107.5.4.3.284980.19951129.170916.11"
+        )
+        self.assertEqual(
+            instance._series_uid, "1.3.12.2.1107.5.4.3.4975316777216.19951114.94101.17"
+        )
+        self.assertEqual(
+            instance._study_uid, "1.3.12.2.1107.5.4.3.4975316777216.19951114.94101.16"
+        )
+        # getter methods should return the same values
+        self.assertEqual(instance.instance_uid, instance._instance_uid)
+        self.assertEqual(instance.series_uid, instance._series_uid)
+        self.assertEqual(instance.study_uid, instance._study_uid)
+
+    def test_append_to_series_tar(self):
+        instance = Instance(os.path.join(self.test_data_dir, "small_multiframe.dcm"))
+        with tarfile.open("series.tar", "w") as tar:
+            instance.append_to_series_tar(tar)
