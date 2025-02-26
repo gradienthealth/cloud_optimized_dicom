3575f7cb6b5141d8996fc96de522c1ee7f2898fa
Cal Nightingale <cal.nightingale@gmail.com>
2025-02-25 20:04:15 -0700

    cod object first test

---
+++ cloud_optimized_dicom/tests/test_cod_object.py
@@ -0,0 +1,29 @@
+import unittest
+
+from cloud_optimized_dicom.cod_object import CODObject
+
+
+class TestCODObject(unittest.TestCase):
+    def test_properties(self):
+        """Test tar_uri, metadata_uri, index_uri, and __str__"""
+        cod_object = CODObject(
+            datastore_path="gs://my-bucket/my-datastore",
+            client=None,
+            study_uid="1.2.3.4",
+            series_uid="5.6.7.8",
+        )
+        self.assertEqual(cod_object.datastore_path, "gs://my-bucket/my-datastore")
+        self.assertEqual(
+            cod_object.tar_uri, "gs://my-bucket/my-datastore/1.2.3.4/5.6.7.8.tar"
+        )
+        self.assertEqual(
+            cod_object.metadata_uri,
+            "gs://my-bucket/my-datastore/1.2.3.4/5.6.7.8/metadata.json",
+        )
+        self.assertEqual(
+            cod_object.index_uri,
+            "gs://my-bucket/my-datastore/1.2.3.4/5.6.7.8/index.sqlite",
+        )
+        self.assertEqual(
+            str(cod_object), "CODObject(gs://my-bucket/my-datastore/1.2.3.4/5.6.7.8)"
+        )
