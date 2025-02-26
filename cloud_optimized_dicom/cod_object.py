2f7e6a992288aad813fbb84857e62d96cdbe02ac
Cal Nightingale <cal.nightingale@gmail.com>
2025-02-25 19:59:35 -0700

    cod_obj initial commit

---
+++ cloud_optimized_dicom/cod_object.py
@@ -0,0 +1,46 @@
+from google.cloud import storage
+
+
+class CODObject:
+    """
+    A Logical representation of a DICOM series stored in the cloud.
+
+    NOTE: The UIDs provided on initialization are used directly in COD URIs (e.g. `<datastore_path>/<study_uid>/<series_uid>.tar`)
+    SO, if these UIDs are supposed to be de-identified, the caller is responsible for this de-identification.
+
+    Parameters:
+        datastore_path: str - The path to the datastore file for this series.
+        client: storage.Client - The client to use to interact with the datastore.
+        study_uid: str - The study_uid of the series.
+        series_uid: str - The series_uid of the series.
+    """
+
+    def __init__(
+        self,
+        datastore_path: str,
+        client: storage.Client,
+        study_uid: str,
+        series_uid: str,
+    ):
+        self.datastore_path = datastore_path
+        self.client = client
+        self.study_uid = study_uid
+        self.series_uid = series_uid
+
+    @property
+    def tar_uri(self) -> str:
+        """The URI of the tar file for this series in the COD datastore."""
+        return f"{self.datastore_path}/{self.study_uid}/{self.series_uid}.tar"
+
+    @property
+    def metadata_uri(self) -> str:
+        """The URI of the metadata file for this series in the COD datastore."""
+        return f"{self.datastore_path}/{self.study_uid}/{self.series_uid}/metadata.json"
+
+    @property
+    def index_uri(self) -> str:
+        """The URI of the index file for this series in the COD datastore."""
+        return f"{self.datastore_path}/{self.study_uid}/{self.series_uid}/index.sqlite"
+
+    def __str__(self):
+        return f"CODObject({self.datastore_path}/{self.study_uid}/{self.series_uid})"
