5e07cb0c9c650a96542a7b39fc99c79deaea03dc
Cal Nightingale <cal.nightingale@gmail.com>
2025-04-09 15:21:41 -0400

    series metadata can infer whether to use deid/standard uid keys

--- cloud_optimized_dicom/series_metadata.py
+++ cloud_optimized_dicom/series_metadata.py
@@ -1,11 +1,14 @@
 import gzip
 import json
-from dataclasses import asdict, dataclass, field
+import logging
+from dataclasses import dataclass, field
 from io import BytesIO
 
 from google.cloud import storage
 
 from cloud_optimized_dicom.instance import Instance
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -17,27 +20,56 @@ class SeriesMetadata:
         series_uid (str): The series UID of this series (should match `CODObject.series_uid`)
         instances (dict[str, Instance]): Mapping of instance UID to Instance object
         custom_tags (dict): Any additional user defined data
+        _is_hashed (bool): Private property indicating whether the series uses de-identified UIDs.
+        If loading existing metadata, this is inferred by the presence of the key `deid_study_uid` as opposed to `study_uid`.
+        If creating new metadata, this is inferred by the presence/absence of `instance.uid_hash_func` for any instances that have been added.
     """
 
     study_uid: str
     series_uid: str
     instances: dict[str, Instance] = field(default_factory=dict)
     custom_tags: dict = field(default_factory=dict)
+    _is_hashed: bool = False
+
+    def _infer_is_hashed(self):
+        """It is possible to infer that a series is hashed in the following ways:
+        1. Pre-existing metadata: `_is_hashed` was already set to True on load, because `deid_study_uid` was present (instead of `study_uid`)
+        2. Creating new metadata: all of the instances have the same `uid_hash_func` (which is not None)
+        """
+        # case 1: already set
+        if self._is_hashed:
+            return
+        # if there are no instances, we cannot infer if the series is hashed
+        if len(self.instances) == 0:
+            logger.warning("Series has no instances, cannot infer if it is hashed")
+            return
+        # case 2: new metadata
+        hash_funcs = set(instance.uid_hash_func for instance in self.instances.values())
+        # we should never see multiple different hash functions for a series
+        if len(hash_funcs) != 1:
+            raise ValueError(
+                "Series has instances with multiple different uid_hash_funcs, which should be impossible"
+            )
+        # if the hash function is not None, then the series is hashed
+        self._is_hashed = hash_funcs[0] is not None
 
     def to_dict(self) -> dict:
         # TODO version handling once we have a new version
-        # TODO existing gradient uses "deid_{study/series}_uid"... how to reconcile?
-        return {
-            "study_uid": self.study_uid,
-            "series_uid": self.series_uid,
+        # prior to saving, make sure _is_hashed is set correctly
+        self._infer_is_hashed()
+        study_uid_key = "deid_study_uid" if self._is_hashed else "study_uid"
+        series_uid_key = "deid_series_uid" if self._is_hashed else "series_uid"
+        base_dict = {
+            study_uid_key: self.study_uid,
+            series_uid_key: self.series_uid,
             "cod": {
                 "instances": {
                     instance_uid: instance.to_cod_dict_v1()
                     for instance_uid, instance in self.instances.items()
                 },
             },
-            "thumbnail": asdict(self.thumbnail) if self.thumbnail else None,
         }
+        return {**base_dict, **self.custom_tags}
 
     def to_gzipped_json(self) -> bytes:
         """Convert from SeriesMetadata -> dict -> JSON -> bytes -> gzip"""
@@ -57,9 +89,11 @@ class SeriesMetadata:
         """Class method to create an instance from a dictionary."""
         # retrieve the study and series UIDs (might be de-identified)
         if "deid_study_uid" in series_metadata_dict:
+            is_hashed = True
             study_uid = series_metadata_dict.pop("deid_study_uid")
             series_uid = series_metadata_dict.pop("deid_series_uid")
         else:
+            is_hashed = False
             study_uid = series_metadata_dict.pop("study_uid")
             series_uid = series_metadata_dict.pop("series_uid")
 
@@ -78,6 +112,7 @@ class SeriesMetadata:
             series_uid=series_uid,
             instances=instances,
             custom_tags=custom_tags,
+            _is_hashed=is_hashed,
         )
 
     @classmethod
