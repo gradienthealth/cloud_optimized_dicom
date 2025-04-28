531c168433dc9cc66031e32db2abe8aca5a04a74
Cal Nightingale <cal.nightingale@gmail.com>
2025-04-28 17:23:27 -0400

    removed siskin from docstring

--- cloud_optimized_dicom/utils.py
+++ cloud_optimized_dicom/utils.py
@@ -101,7 +101,7 @@ def delete_uploaded_blobs(client: storage.Client, uris_to_delete: list[str]):
     """
     Helper method used by tests to delete blobs they have created, resetting the test
     environment for a subsequent test. Takes a GCS client and a list of GCS uris to delete.
-    These URIs should be folders (e.g. 'gs://siskin-172863-test-data/concat-output'), and
+    These URIs should be folders (e.g. 'gs://some-bucket/concat-output'), and
     this method will delete everything in the folder
     """
     for gcs_uri in uris_to_delete:
