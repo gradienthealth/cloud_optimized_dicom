d61b23ef74ed40a1235a4ba0e202bc7ec17338a9
Cal Nightingale <cal.nightingale@gmail.com>
2025-05-08 16:44:46 -0400

    instances sorted by InstanceNumber, SliceLocation

--- cloud_optimized_dicom/thumbnail.py
+++ cloud_optimized_dicom/thumbnail.py
@@ -1,5 +1,34 @@
+import logging
+
+import pydicom3
+
 from cloud_optimized_dicom.cod_object import CODObject
-from cloud_optimized_dicom.pydicom3 import dcmread
+from cloud_optimized_dicom.instance import Instance
+
+logger = logging.getLogger(__name__)
+
+SORTING_ATTRIBUTES = {"InstanceNumber": "00200013", "SliceLocation": "00201041"}
+
+
+def _sort_instances(instances: list[Instance]) -> list[Instance]:
+    """Attempt to sort instances by instance_number tag. Try slice_location if that fails.
+    If both fail, return the instances in the order they were fetched, and log a warning.
+    """
+    # if there's only one instance, return it as is
+    if len(instances) <= 1:
+        return instances
+    # attempt to sort by by each attribute in SORTING_ATTRIBUTES
+    for tag in SORTING_ATTRIBUTES.values():
+        # do not attempt sorting if any instances are missing the tag
+        if any(tag not in instance.metadata for instance in instances):
+            continue
+        # sortable attributes are expected to be stored in metadata as "tag": {"vr":"VR","Value":[some_value]}
+        return sorted(instances, key=lambda x: x.metadata[tag]["Value"][0])
+    # if no sorting was successful, return the instances in the order they were fetched
+    logger.warning(
+        f"Unable to sort instances by any known sorting attributes ({', '.join(SORTING_ATTRIBUTES.keys())})"
+    )
+    return instances
 
 
 def generate_thumbnail(cod_obj: CODObject, dirty: bool = False):
@@ -8,11 +37,11 @@ def generate_thumbnail(cod_obj: CODObject, dirty: bool = False):
     if cod_obj.tar_is_empty:
         cod_obj.pull_tar(dirty=dirty)
 
-    for instance in cod_obj.get_metadata(dirty=dirty).instances.values():
-        print(instance.dicom_uri)
-        with instance.open() as f:
-            ds = dcmread(f)
-            print(ds.StudyInstanceUID)
-            print(ds.SeriesInstanceUID)
-            print(ds.SOPInstanceUID)
-    return None
+    # fetch list of instances
+    instances = cod_obj.get_metadata(dirty=dirty).instances.values()
+    # filter out instances without pixel data
+    instances = [instance for instance in instances if instance.has_pixeldata]
+    instances = _sort_instances(instances)
+    with instances[0].open() as f:
+        ds = pydicom3.dcmread(f)
+        print(ds.StudyInstanceUID)
