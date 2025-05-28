e6090bd77380d30c6a1935c4aa2f98cece2af547
Cal Nightingale <cal.nightingale@gmail.com>
2025-05-28 12:05:23 -0400

    truncate test

--- cloud_optimized_dicom/tests/test_truncate.py
+++ cloud_optimized_dicom/tests/test_truncate.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import tarfile
 import unittest
 
 from google.api_core.client_options import ClientOptions
@@ -11,9 +12,66 @@ from cloud_optimized_dicom.utils import delete_uploaded_blobs
 
 
 class TestTruncate(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.test_data_dir = os.path.join(os.path.dirname(__file__), "test_data")
+        cls.client = storage.Client(
+            project="gradient-pacs-siskin-172863",
+            client_options=ClientOptions(
+                quota_project_id="gradient-pacs-siskin-172863"
+            ),
+        )
+        cls.datastore_path = "gs://siskin-172863-temp/cod_tests/dicomweb"
+        logging.basicConfig(level=logging.INFO)
+
+    def setUp(self):
+        # ensure clean test directory prior to test start
+        delete_uploaded_blobs(self.client, [self.datastore_path])
+
     def test_truncate(self):
         """
         Test that a cod object can be successfully truncated.
+        """
+        instance1 = Instance(
+            dicom_uri=os.path.join(
+                self.test_data_dir,
+                "series",
+                "1.2.826.0.1.3680043.8.498.22997958494980951977704130269567444795.dcm",
+            )
+        )
+        instance2 = Instance(
+            dicom_uri=os.path.join(
+                self.test_data_dir,
+                "series",
+                "1.2.826.0.1.3680043.8.498.28109707839310833322020505651875585013.dcm",
+            )
+        )
+        cod_obj = CODObject(
+            datastore_path=self.datastore_path,
+            client=self.client,
+            study_uid=instance1.study_uid(),
+            series_uid=instance1.series_uid(),
+            lock=False,
+        )
+        append_result = cod_obj.append(instances=[instance1], dirty=True)
+        self.assertEqual(len(append_result.new), 1)
+        truncate_result = cod_obj.truncate(instances=[instance2], dirty=True)
+        self.assertEqual(len(truncate_result.new), 1)
+        self.assertEqual(truncate_result.new[0], instance2)
+        # cod object should ONLY contain the new instance
+        self.assertEqual(
+            list(cod_obj.get_metadata(dirty=True).instances.values()), [instance2]
+        )
+        with tarfile.open(cod_obj.tar_file_path, "r") as tar:
+            self.assertEqual(len(tar.getmembers()), 1)
+            self.assertEqual(
+                tar.getmembers()[0].name, f"instances/{instance2.instance_uid()}.dcm"
+            )
+
+    def test_truncate_remote(self):
+        """
+        Test that a cod object can be successfully truncated from a remote cod object.
         """
 
 
