5d0886106c9c5794370cc2fc876ed511698d380e
Cal Nightingale <cal.nightingale@gmail.com>
2025-05-29 13:05:21 -0400

    series and instance level GET req support

--- cloud_optimized_dicom/dicomweb.py
+++ cloud_optimized_dicom/dicomweb.py
@@ -1,6 +1,89 @@
+from google.cloud import storage
+
+from cloud_optimized_dicom.cod_object import CODObject
+
+
 # Reference: https://www.dicomstandard.org/docs/librariesprovider2/dicomdocuments/dicom/wp-content/uploads/2018/04/dicomweb-cheatsheet.pdf
-def handle_dicomweb_request(request: str):
+def parse_dicomweb_request(request: str):
     """
     Handle a dicomweb request of format "GET {s}/studies/{study}/series/{series}"
     """
-    print("Received dicomweb request: ", request)
+    assert request.startswith("GET "), "Only GET requests are currently supported"
+    uri = request.replace("GET", "").strip()
+    assert uri.startswith("gs://"), "Only gs:// URIs are supported"
+    assert "?" not in uri, "Query parameters are not supported"
+    assert "/studies/" in uri, "Invalid request format"
+    subpath_index = uri.index("studies/")
+    datastore_uri = uri[:subpath_index].rstrip("/")
+    subpath = uri[subpath_index:]
+    subpath_parts = subpath.split("/")
+    assert (
+        len(subpath_parts) % 2 == 0
+    ), f"Expected even number of parts in subpath: {subpath}"
+    # map odd indices to subsequet even ones in dict
+    request_dict = {"datastore_uri": datastore_uri}
+    for i in range(0, len(subpath_parts), 2):
+        request_dict[subpath_parts[i]] = subpath_parts[i + 1]
+    return request_dict
+
+
+def handle_dicomweb_request(request: str, client: storage.Client):
+    """
+    Handle a dicomweb request of format "GET {s}/studies/{study}/series/{series}"
+    """
+    request_dict = parse_dicomweb_request(request)
+    if "frames" in request_dict:
+        return _handle_frame_level_request(request_dict, client)
+    if "instances" in request_dict:
+        return _handle_instance_level_request(
+            datastore_uri=request_dict["datastore_uri"],
+            study_uid=request_dict["studies"],
+            series_uid=request_dict["series"],
+            instance_uid=request_dict["instances"],
+            client=client,
+        )
+    if "series" in request_dict:
+        return _handle_series_level_request(
+            datastore_uri=request_dict["datastore_uri"],
+            study_uid=request_dict["studies"],
+            series_uid=request_dict["series"],
+            client=client,
+        )
+    if "studies" in request_dict:
+        return _handle_study_level_request(request_dict, client)
+    raise ValueError("Invalid request format")
+
+
+def _handle_frame_level_request(request_dict: dict, client: storage.Client):
+    raise NotImplementedError("frame level requests not yet supported")
+
+
+def _handle_instance_level_request(
+    datastore_uri: str,
+    study_uid: str,
+    series_uid: str,
+    instance_uid: str,
+    client: storage.Client,
+):
+    series_dict = _handle_series_level_request(
+        datastore_uri, study_uid, series_uid, client
+    )
+    return series_dict["instances"][instance_uid]["metadata"]
+
+
+def _handle_series_level_request(
+    datastore_uri: str, study_uid: str, series_uid: str, client: storage.Client
+):
+    cod_obj = CODObject(
+        datastore_path=datastore_uri,
+        client=client,
+        study_uid=study_uid,
+        series_uid=series_uid,
+        lock=False,
+        create_if_missing=False,
+    )
+    return cod_obj.get_metadata(dirty=True).to_dict()["cod"]
+
+
+def _handle_study_level_request(request_dict: dict, client: storage.Client):
+    raise NotImplementedError("study level requests not yet supported")
--- cloud_optimized_dicom/tests/test_dicomweb.py
+++ cloud_optimized_dicom/tests/test_dicomweb.py
@@ -18,7 +18,7 @@ class TestDicomweb(unittest.TestCase):
             ),
         )
 
-    def test_series_existence(self):
+    def test_get_series(self):
         """
         Test that series existence can be queried via dicomweb standard
         """
@@ -30,7 +30,23 @@ class TestDicomweb(unittest.TestCase):
             "1.2.826.0.1.3680043.8.498.89840699185761593370876698622882853150",
         )
         request = f"GET {series_uri}"
-        handle_dicomweb_request(request)
+        result = handle_dicomweb_request(request, self.client)
+        # some basic checks to make sure the result is valid
+        self.assertIn("instances", result)
+
+    def test_get_instance(self):
+        instance_uri = os.path.join(
+            self.datastore_path,
+            "studies",
+            "1.2.826.0.1.3680043.8.498.18783474219392509401504861043428417882",
+            "series",
+            "1.2.826.0.1.3680043.8.498.89840699185761593370876698622882853150",
+            "instances",
+            "1.2.826.0.1.3680043.8.498.10368404844741579486264078308290534273",
+        )
+        request = f"GET {instance_uri}"
+        result = handle_dicomweb_request(request, self.client)
+        self.assertIn("0020000D", result)
 
 
 if __name__ == "__main__":
